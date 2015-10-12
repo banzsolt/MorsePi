cc4cef408b8eb48281d5149a306e4bae5c1f71e1
Zsolt Ban <banzsolt94@gmail.com>
2015-10-12 22:40:55 +0100

    Version 1 of braking lights

--- morse.py
+++ morse.py
@@ -4,9 +4,9 @@ import multiprocessing
 import sys
 
 args = sys.argv
-parallel = True
+parallel = False
 same_output = True
-speed = 0.1
+speed = 0.025
 data = {'3': 'SOS',
         '5': '',
         '7': '',
@@ -84,6 +84,20 @@ def process(the_text, to_pin):
                     long(to_pin)
                 time.sleep(speed)
 
+def process_arguments():
+    global args
+    if len(args) == 0:
+        return
+    print(args)
+    information = None
+    last_option = None
+    for argument in args:
+        if '-' in argument:
+            information[string(argument)] = None
+            last_option = string(argument)
+        if last_option is not None:
+            information[last_option] = argument
+    return  information
 
 def setup():
     global data
@@ -106,17 +120,87 @@ def short(to_pin):
     GPIO.output(to_pin, GPIO.LOW)
 
 
+def on(to_pin):
+    global speed
+    GPIO.output(to_pin, GPIO.HIGH)
+    time.sleep(speed)
+
+
+def off(to_pin):
+    global speed
+    GPIO.output(to_pin, GPIO.LOW)
+    time.sleep(speed)
+    
+    
 if __name__ == '__main__':
     global data, parallel, same_output, args
     setup()
+    test = process_arguments()
+    print(test)
 
     while 1:
         processes = []
-        for pin_no, text in data.iteritems():
-            output = text
-            if same_output:
-                output = data['3']
-            processes.append(multiprocessing.Process(target=process, args=(output, int(pin_no),)))
+        helper = {'1': 7,
+                  '2': 10,
+                  '3': 11,
+                  '4': 12,
+                  '5': 13}
+        # 1,2,3,4,5,5,4,3,2,1 - run till the end and back
+        sequence = [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+        
+        for repeat in range (0, 3):
+            for pin in sequence:
+                processes.append(multiprocessing.Process(target=process, args=('e', helper[str(pin)],)))    
+        
+        # nice snaky flash
+        for repeat in range (0,3):
+            i = 1
+            while i <= 5:
+                processes.append(multiprocessing.Process(target=on, args=(helper[str(i)],)))
+                i = i+1
+                
+            i = 1
+            while i <= 5:
+                processes.append(multiprocessing.Process(target=off, args=(helper[str(i)],)))
+                i = i+1
+                
+        # nice snaky flash backwards
+        for repeat in range (0,3):
+            i = 5
+            while i > 0:
+                processes.append(multiprocessing.Process(target=on, args=(helper[str(i)],)))
+                i = i-1
+                
+            i = 5
+            while i > 0:
+                processes.append(multiprocessing.Process(target=off, args=(helper[str(i)],)))
+                i = i-1
+                
+        for repeat in range (0,2):  
+            processes.append(multiprocessing.Process(target=on, args=(helper['3'],)))
+            processes.append(multiprocessing.Process(target=on, args=(helper['2'],)))
+            processes.append(multiprocessing.Process(target=on, args=(helper['4'],)))
+            processes.append(multiprocessing.Process(target=on, args=(helper['1'],)))
+            processes.append(multiprocessing.Process(target=on, args=(helper['5'],)))
+            
+            processes.append(multiprocessing.Process(target=off, args=(helper['5'],)))
+            processes.append(multiprocessing.Process(target=off, args=(helper['1'],)))
+            processes.append(multiprocessing.Process(target=off, args=(helper['4'],)))
+            processes.append(multiprocessing.Process(target=off, args=(helper['2'],)))
+            processes.append(multiprocessing.Process(target=off, args=(helper['3'],)))
+        
+        for repeat in range(0,5):
+            processes.append(multiprocessing.Process(target=off, args=(helper['1'],)))
+            
+        for pin in range (1, 6):
+            processes.append(multiprocessing.Process(target=on, args=(helper[str(pin)],)))
+            for sleep in range (0,10):
+                processes.append(multiprocessing.Process(target=off, args=(40,)))
+                
+        for pin in range (1, 6):
+            processes.append(multiprocessing.Process(target=off, args=(helper[str(pin)],)))
+        
+                
         i = 0
         while i < len(processes):
             processes[i].start()
